38cf027480baaee9a59154b96a41cc93ffa1aebc
Theo2801 <theologos.bourloglou@gmail.com>
2023-09-06 17:55:09 +0300

    [feat] Add all category images and secondary buttons functionality

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
 const foodCategories = [
-   ['Σουβλάκι', 'souvlakia'],
-   ['Pizza', 'pizza'],
-   ['Κρέπα', 'crepes'],
-   ['Κινέζικο', 'chinese'],
-   ['Burger', 'burgers'],
-   ['Sushi', 'sushi'],
-   ['Μαγειρευτό', 'cooked'],
-   ['Ζυμαρικό', 'pasta'],
-   ['Μεξικάνικο', 'mexican'],
-   ['Βάφλα', 'waffle'],
-   ['Ινδικό', 'indian'],
-   ['Hot Dog', 'hot_dog'],
-   ['Σαλάτα', 'salads'],
-   ['Σαντουιτς', 'sandwich'],
-   ['Ψητά - Grill', 'psita'],
-   ['Πεϊνιρλί', 'peinirli'],
-   ['Brunch', 'brunch'],
-   ['Vegetarian', 'vegetarian'],
-   ['Vegan', 'vegan'],
-   ['Θαλασσινό', 'seafood'],
-   ['Ασιατικό', 'asian'],
-   ['Ιταλικό', 'italian'],
-   ['Λουκουμάδες', 'loukoumades'],
-   ['Κοτόπουλο', 'kotopoulo'],
+   ['Σουβλάκι', 'souvlaki' ,'', ''],
+   ['Pizza', 'pizza' ,'', ''],
+   ['Κρέπα', 'crepe' ,'', ''],
+   ['Κινέζικο', 'chinese' ,'', ''],
+   ['Burger', 'burgers' ,'', ''],
+   ['Sushi', 'sushi' ,'', ''],
+   ['Μαγειρευτό', 'cooked' ,'', ''],
+   ['Ζυμαρικό', 'pasta' ,'', ''],
+   ['Μεξικάνικο', 'mexican' ,'', ''],
+   ['Βάφλα', 'waffle' ,'', ''],
+   ['Ινδικό', 'indian' ,'', ''],
+   ['Hot Dog', 'hotdog' ,'', ''],
+   ['Σαλάτα', 'salad' ,'', ''],
+   ['Σαντουιτς', 'sandwich' ,'', ''],
+   ['Ψητά - Grill', 'grill' ,'', ''],
+   ['Πεϊνιρλί', 'peinirli' ,'', ''],
+   ['Brunch', 'brunch' ,'', ''],
+   ['Vegetarian', 'vegetarian' ,'', ''],
+   ['Vegan', 'vegan' ,'', ''],
+   ['Θαλασσινό', 'seafood' ,'', ''],
+   ['Ασιατικό', 'asian' ,'', ''],
+   ['Ιταλικό', 'italian' ,'', ''],
+   ['Λουκουμάδες', 'loukoumades' ,'', ''],
+   ['Κοτόπουλο', 'chicken' ,'', ''],
 ];
 
 function generateRandomCategory() {
@@ -31,6 +31,7 @@ function generateRandomCategory() {
 
 
 document.addEventListener('DOMContentLoaded', () => {
+   let initial_click = true;
    const button = document.getElementById('button');
    const button_text = document.getElementById('button-text');
    const button_again = document.getElementById('button-again');
@@ -38,18 +39,24 @@ document.addEventListener('DOMContentLoaded', () => {
    const img = document.getElementById('category-img');
    const efood = document.getElementById('e-food');
    const wolt = document.getElementById('wolt');
+   const efood_link = 'https://www.e-food.gr/shops?categories=';
+   const wolt_link = 'https://wolt.com/el/grc/*/category/';
 
    button.addEventListener('click', () => {
-      if (efood.classList.contains('hidden') || wolt.classList.contains('hidden')) {
+      if (initial_click) {
          efood.classList.remove('hidden')
          wolt.classList.remove('hidden')
-         button_text.innerHTML = 'Μήπως θέλεις';
          button_again.classList.remove('hidden');
+         button_text.innerHTML = 'Μήπως θέλεις';
+
+         initial_click = false;
       }
 
 
-      const index = generateRandomCategory()
+      const index = generateRandomCategory();
       category_title.innerHTML = foodCategories[index][0] + ';';
-      img.src = '../icons/' + foodCategories[index][1]  + '.png';
+      img.src = '../icons/' + foodCategories[index][1]  + '.webp';
+      efood.href = efood_link + foodCategories[index][1];
+      wolt.href = wolt_link + foodCategories[index][1];
    });
 });
